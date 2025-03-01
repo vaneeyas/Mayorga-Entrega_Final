d96faa4e13f0fa749a99b5ea5fb37048c8780f41
Vanesa Yasmin Mayorga <vanesa.yasmin21@gmail.com>
2025-02-28 23:18:47 -0300

    Iniciar Sesión

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
+from autenticacion.views import login
 
 urlpatterns = [
-    #path('login/', login, name="login"),
+    path('login/', login, name="login"),
     path('logout/', LogoutView.as_view(template_name="autenticacion/logout.html"), name="logout"),
     #path('registro/', registro, name="registro"),
 ]
--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as django_login
 
-# Create your views here.
+def login(request):
+    if request.method == "POST":
+        formulario = AuthenticationForm(request, data=request.POST)
+        if formulario.is_valid():
+            usuario = formulario.get_user()
+            django_login(request, usuario)
+            return redirect("index")
+    else:
+        formulario = AuthenticationForm()  
+    return render(request, "autenticacion/login.html", {'formulario': formulario})
